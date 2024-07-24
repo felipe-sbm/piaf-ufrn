c9a21331d7797aa0dc87f0c873c05c888db8c530
felipe-sbm <felipesbm2005@gmail.com>
2024-07-24 09:31:52 -0300

    Editando rotas e API

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+      // enableRemoteModule: false, // Não é mais necessário nas versões mais recentes do Electron
+    },
   });
 
-  win.loadFile('pages/index.html');
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.insertCSS(path.join(__dirname, 'styles.css'));
+  win.loadFile("pages/index.html"); // Carrega o arquivo HTML
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.insertCSS(path.join(__dirname, "global.css")); // Insere o CSS (NÃO ESTÁ FUNCIONANDO!)
   });
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow(); // Cria a janela
+});
+
+ipcMain.handle("dialog:openFile", async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ["openFile"],
+    filters: [{ name: "Planilhas", extensions: ["xlsx", "xls"] }],
+  });
+  if (canceled) {
+    return;
+  } else {
+    return filePaths[0];
+  }
+});
+
+ipcMain.handle("dialog:saveFile", async () => {
+  const { canceled, filePath } = await dialog.showSaveDialog({
+    filters: [{ name: "Planilhas", extensions: ["xlsx"] }],
+  });
+  if (canceled) {
+    return;
+  } else {
+    return filePath;
+  }
 });
--- preload.js
+++ preload.js
@@ -1,5 +1,7 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  handleFileUpload: (callback) => ipcRenderer.on('file-uploaded', callback)
+contextBridge.exposeInMainWorld("electronAPI", {
+  handleFileUpload: (callback) => ipcRenderer.on("file-uploaded", callback), // Recebe o arquivo enviado pelo processo principal
+  handleFileRead: () => ipcRenderer.invoke("dialog:openFile"), // Abre a janela de diálogo para ler um arquivo
+  handleFIleSave: () => ipcRenderer.invoke("dialog:saveFile"), // Abre a janela de diálogo para salvar um arquivo
 });
